256ba733e90f80a825cbc6cabec1178fce6b5326
mosarrof <hossensakib27@gmail.com>
2022-03-02 21:06:53 +0600

    ready for deployment3

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ig$#+0xpy4ibqc!$km!4*l7x%nrw7+w^%vsx)#jur8!x$_(37f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', 'django-react-my-proshop.herokuapp.com']
 
 
